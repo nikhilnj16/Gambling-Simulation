793db439c2c0924a7af29bf0c7b0c4349af7090c
Nikhil <nikiljana123@gmail.com>
2024-01-25 12:09:59 +0530

    added: Exit

--- Gambling Simulation/src/com/bridgelabz/solution/GamblingSimulation.java
+++ Gambling Simulation/src/com/bridgelabz/solution/GamblingSimulation.java
@@ -3,11 +3,13 @@ package com.bridgelabz.solution;
 public class GamblingSimulation {
     public int stake;
     public int bet;
+    public int noOfGames;
 
 
     public GamblingSimulation() {
         this.stake = 100;
         this.bet = 1;
+        this.noOfGames = 10;
     }
 
     public static Boolean WinOrLoss(){
@@ -18,32 +20,30 @@ public class GamblingSimulation {
             return false;
         }
     }
-    public static void StakePrice(int stake, int bet){
-        if (WinOrLoss()){
-            stake+=bet;
+    public static void StakePrice(int stake, int bet, int noOfGames){
+        String result;
+        for (int i=0; i<noOfGames; i++) {
+            if (WinOrLoss()) {
+                result = "win";
+                stake += bet;
 
+            } else {
+                result = "lost";
+                stake -= bet;
+            }
+            if (stake <= 50 || stake >= 150){
+                System.out.println("Not able to Play furthur");
+                break;
+            }
+            System.out.println("result: " + result);
+            System.out.println("stakeValue : " + stake);
         }
-        else {
-            stake-=bet;
-        }
-        System.out.println("stake : " + stake);
-    }
-    public int getStake(){
-        return stake;
-    }
-    public int getBet(){
-        return bet;
     }
 
-    @Override
-    public String toString() {
-        return "result: " + WinOrLoss();
-
-    }
 
     public static void main(String[] args) {
         GamblingSimulation obj = new GamblingSimulation();
-        obj.StakePrice(100, 1);
+        obj.StakePrice(100, 1, 100);
         System.out.println(obj);
 
 
