fc1ad2ef1837522b188b006ec3adedfdda47f989
Nikhil <nikiljana123@gmail.com>
2024-01-25 11:56:50 +0530

    added: stakestatus

--- Gambling Simulation/src/com/bridgelabz/solution/GamblingSimulation.java
+++ Gambling Simulation/src/com/bridgelabz/solution/GamblingSimulation.java
@@ -10,24 +10,42 @@ public class GamblingSimulation {
         this.bet = 1;
     }
 
-    public static String WinOrLoss(){
+    public static Boolean WinOrLoss(){
         if (Math.random()<0.5){
-            return "Win";
+            return true;
         }
         else {
-            return "Lose";
+            return false;
         }
+    }
+    public static void StakePrice(int stake, int bet){
+        if (WinOrLoss()){
+            stake+=bet;
+
+        }
+        else {
+            stake-=bet;
+        }
+        System.out.println("stake : " + stake);
+    }
+    public int getStake(){
+        return stake;
+    }
+    public int getBet(){
+        return bet;
     }
 
     @Override
     public String toString() {
         return "result: " + WinOrLoss();
+
     }
 
     public static void main(String[] args) {
         GamblingSimulation obj = new GamblingSimulation();
-
+        obj.StakePrice(100, 1);
         System.out.println(obj);
+
 
     }
 }
