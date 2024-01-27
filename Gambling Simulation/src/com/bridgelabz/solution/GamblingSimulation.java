d20f26109092f1ba4915ab049521ff85a5c05bc1
Nikhil <nikiljana123@gmail.com>
2024-01-27 20:45:38 +0530

    added : status after 20 days

--- Gambling Simulation/src/com/bridgelabz/solution/GamblingSimulation.java
+++ Gambling Simulation/src/com/bridgelabz/solution/GamblingSimulation.java
@@ -1,16 +1,7 @@
 package com.bridgelabz.solution;
 
 public class GamblingSimulation {
-    public int stake;
-    public int bet;
-    public int noOfGames;
 
-
-    public GamblingSimulation() {
-        this.stake = 100;
-        this.bet = 1;
-        this.noOfGames = 10;
-    }
 
     public static Boolean WinOrLoss(){
         if (Math.random()<0.5){
@@ -20,46 +11,42 @@ public class GamblingSimulation {
             return false;
         }
     }
-    public void Status(int totalstake){
-        if (totalstake < 100){
-            System.out.println("Lost: " + (100-totalstake));
-        }
-        else if (totalstake > 100){
-            System.out.println("Won: " + (totalstake-100));
-        }
-        else {
-            System.out.println("No profit no loss");
-        }
-    }
-    public void StakePrice(int stake, int bet, int noOfGames){
+    
+    public static int simulateGambling(int initialStake, int betAmount, int noOfDays){
         String result;
-        int totalstake=stake;
-        for (int i=0; i<noOfGames; i++) {
-            if (WinOrLoss()) {
-                result = "win";
-                totalstake += bet;
+        int totalAmountWonOrLost=0;
+        for (int day=1; day<=noOfDays; day++) {
+            int currentStake = initialStake;
 
-            } else {
-                result = "lost";
-                totalstake -= bet;
+            while (currentStake > 50 && currentStake < 150) {
+
+                if (WinOrLoss()) {
+                    result = "win";
+                    currentStake += betAmount;
+
+                } else {
+                    result = "lost";
+                    currentStake -= betAmount;
+                }
             }
-            if (totalstake <= 50 || totalstake >= 150){
-                System.out.println("Not able to Play furthur");
-                break;
-            }
-            if (i==20){
-                System.out.println("Status after 20 days");
-                Status(totalstake);
-            }
-            System.out.println("result: " + result);
-            System.out.println("stakeValue : " + stake);
+            int amountWonOrLost = currentStake - initialStake;
+            totalAmountWonOrLost+=amountWonOrLost;
+
+            System.out.println("Day " + day + ": Amount Won or Lost - $ " + amountWonOrLost);
+
         }
+        return totalAmountWonOrLost;
     }
 
 
     public static void main(String[] args) {
-        GamblingSimulation obj = new GamblingSimulation();
-        obj.StakePrice(100, 1, 25);
+        int initialStake = 100;
+        int betAmount = 1;
+        int noOfDays = 20;
+
+        int totalAmountWonOrLost = simulateGambling(initialStake, betAmount, noOfDays);
+        System.out.println("Total amount won or lost after 20 days: $" + totalAmountWonOrLost);
+
         
 
 
